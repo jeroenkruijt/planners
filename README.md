d509909a9c073cad3d74655070eceb3e24d77748
jeroen kruyt <jeroen_kruyt@hotmail.com>
2019-03-05 14:24:23 +0100

    * overnachting toegevoegd als extra data. * als er geen aantal aanwezig is dat die eer autocompleet naar 0 dat er altijd iets te zien is.
    Signed-off-by: jeroen kruyt <jeroen_kruyt@hotmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-planners
+        Systeem voor planners
+        
+1. Overzicht van data 
+2. Functie om de achtergrond te veranderdere van de cel 
+3. Opmerking achter elk data te plaatsen 
+4. Indicatie dat er een opmerking achter zit
+5. Opmerking bewerken 
+6. Opmerking verwijderen 
+7. Extra data toevoegen aan hand van dropdowns 
--- db/db.connect.php
+++ db/db.connect.php
@@ -17,3 +17,16 @@ $conn = mysqli_connect($host, $user, $pass, $db);
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
+
+//// db NAS
+//$servername3 = "192.168.20.201:3307";
+//$username3 = "klantenportaal";
+//$password3 = "R54#sec:Cod";
+//$dbname3 = "planningssysteem";
+//
+//// connect to NAS
+//$conn = new mysqli($servername3, $username3, $password3, $dbname3);
+//// Check connection
+//if ($conn->connect_error) {
+//    die("Connection failed: " . $conn->connect_error);
+//}
